1c0a76f5e4857e35647f4779f294f06895327869
Damien Milon <37899667+dmilon@users.noreply.github.com>
2021-12-22 19:01:18 +0100

    footer ok

--- assets/js/init_mapbox.js
+++ assets/js/init_mapbox.js
@@ -11,7 +11,8 @@ const initMapbox = () => {
       zoom: 10
     })
 
-    const popup = `
+    // create a popup
+    const popupContent = `
       <p class="ff-open-sans fw-bold mb-0">Thibaud Colombel</p>
       <p class="ff-open-sans fw-bold">Ostéopathe D.O.</p>
       <p class="ff-open-sans mb-0">201 rue des Veloutiers</p>
@@ -19,17 +20,22 @@ const initMapbox = () => {
       <p class="ff-open-sans fw-bold"><a href="https://www.google.com/maps/dir//Thibaud+Colombel+-+Ost%C3%A9opathe+D.O./@45.658454,4.6486109,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x47f4ebb545f4a7c7:0x3f25f7330d67b73f!2m2!1d4.7186507!2d45.6584754">Itinéraire</a></p>
     `
 
-    // add a marker
+    const popup = new mapboxgl.Popup({ offset: 25 })
+      .setLngLat([4.718677865667819, 45.6585695829587])
+      .setHTML(popupContent)
+      .addTo(map)
+
+    // add a marker and link the popup
     new mapboxgl.Marker({color: '#22B7CE'})
       .setLngLat([4.718677865667819, 45.6585695829587])
-      .setPopup(
-        new mapboxgl.Popup({ offset: 25 }) // add a popup
-          .setHTML(popup)
-      )
+      .setPopup(popup)
       .addTo(map)
 
     // add controls
     map.addControl(new mapboxgl.NavigationControl())
+
+    // click on the marker to open the popup
+    document.querySelector(".mapboxgl-marker.mapboxgl-marker-anchor-center").click()
   }
 }
 
