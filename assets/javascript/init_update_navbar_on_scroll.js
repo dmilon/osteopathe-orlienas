4b2e1acceae0d59e9771c8f13bf4e8d45e864504
Damien Milon <37899667+dmilon@users.noreply.github.com>
2022-01-16 18:30:48 +0100

    restore js from master

--- assets/javascript/init_update_navbar_on_scroll.js
+++ assets/javascript/init_update_navbar_on_scroll.js
@@ -1,25 +1,19 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar')
   const triggerElement = document.querySelector('.navbar-trigger')
-  if (navbar) {
+  if (navbar && triggerElement) {
     const navbarCta = navbar.querySelector('#navbar-cta')
     const brandText = navbar.querySelector('.brand-text')
-    if (triggerElement) {
-      window.addEventListener('scroll', () => {
-        const triggerElementRectangle = triggerElement.getBoundingClientRect()
-        if (triggerElementRectangle.bottom <= navbar.offsetHeight) {
-          brandText.classList.add('d-none')
-          navbarCta.classList.remove('d-none')
-        } else {
-          brandText.classList.remove('d-none')
-          navbarCta.classList.add('d-none')
-        }
-      })
-    } else {
-      console.log("hello")
-      brandText.classList.add('d-none')
-      navbarCta.classList.remove('d-none')
-    }
+    window.addEventListener('scroll', () => {
+      const triggerElementRectangle = triggerElement.getBoundingClientRect()
+      if (triggerElementRectangle.bottom <= navbar.offsetHeight) {
+        brandText.classList.add('d-none')
+        navbarCta.classList.remove('d-none')
+      } else {
+        brandText.classList.remove('d-none')
+        navbarCta.classList.add('d-none')
+      }
+    })
   }
 }
 
